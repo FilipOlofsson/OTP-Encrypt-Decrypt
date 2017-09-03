95951d3d7f7ea2086897a8eb9ab7857a7e90696f
FilipOlofsson <te15.filip.olofsson@umea.nti.se>
2017-09-03 14:28:42 +0200

    Started on the encryption algorithm

--- src/OTP.java
+++ src/OTP.java
@@ -1,5 +1,8 @@
+import java.io.File;
+
 public class OTP {
     public static void main(String[] args) {
         GUI gui = new GUI(200, 100, "xd");
+        crypto.encrypt(new File("test.txt"), new File("testkey.txt"));
     }
 }
--- src/crypto.java
+++ src/crypto.java
@@ -1,9 +1,35 @@
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class crypto {
 
     public static void encrypt(File raw, File key) {
+        if(raw.length() > key.length()) {
+            System.out.println("The key needs to be of the same length or longer than the original file.");
+        } else {
+            try {
+                File output = new File("output.txt");
+                BufferedReader rawReader = new BufferedReader(new FileReader(raw));
+                BufferedReader keyReader = new BufferedReader(new FileReader(key));
+                BufferedWriter outputWriter = new BufferedWriter(new FileWriter(output));
 
+                int rawRead;
+                int keyRead;
+
+
+
+                while((rawRead = rawReader.read()) != -1 && (keyRead = keyReader.read()) != -1) {
+                    outputWriter.write((char) (rawRead % 26 + keyRead % 26));
+                }
+
+                outputWriter.close();
+                saveFile(output);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void decrypt(File encrypted, File key) {
